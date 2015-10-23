880d628a815f94a449700ec3134b617a2a9222b3
LinkedSoul <LinkedSoul@users.noreply.github.com>
2015-10-22 22:44:55 -0600

    Update app.js

--- app.js
+++ app.js
@@ -231,3 +231,5 @@ fs.readFile(path.resolve(__dirname, 'config/ipbans.txt'), function (err, data) {
  *********************************************************/
 
 require('./repl.js').start('app', function (cmd) { return eval(cmd); });
+
+global.Bot = require('./bot.js');
